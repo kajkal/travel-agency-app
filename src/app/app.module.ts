f1f0c5e57ac2f9af088cd78bea60208152211237
kajkal <karolesniak@gmail.com>
2019-11-23 16:05:04 +0100

    editor environment setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+    ],
+    providers: [],
+    bootstrap: [ AppComponent ],
 })
-export class AppModule { }
+export class AppModule {
+}
+
+
+function t(
+    a: string,
+    b: string,
+): string {
+    return 'asdsa';
+}

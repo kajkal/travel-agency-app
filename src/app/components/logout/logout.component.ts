f66eeadb786686470855436765a413d9ae2606a1
kajkal <karolesniak@gmail.com>
2019-12-19 01:42:19 +0100

    clear cart on logout

--- src/app/components/logout/logout.component.ts
+++ src/app/components/logout/logout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
+import { ShoppingCart } from '../../models/ShoppingCart';
+import { ShoppingService } from '../../services/shopping/shopping.service';
 
 
 @Component({
@@ -12,10 +14,12 @@ export class LogoutComponent implements OnInit {
     constructor(
         private router: Router,
         private authService: AuthService,
+        private shoppingService: ShoppingService,
     ) {
     }
 
     ngOnInit() {
+        this.shoppingService.shoppingCart$.next(ShoppingCart.emptyShoppingCart);
         this.authService.logout();
         this.router.navigate([ '/sign-in' ], { replaceUrl: true });
     }

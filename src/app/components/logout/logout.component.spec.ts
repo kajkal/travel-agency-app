b7cb854806f932a90c1234bd823002945847bbe0
kajkal <karolesniak@gmail.com>
2020-01-30 18:17:58 +0100

    Add logout component tests

--- src/app/components/logout/logout.component.spec.ts
+++ src/app/components/logout/logout.component.spec.ts
@@ -1,26 +1,62 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LogoutComponent } from './logout.component';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
+import { ShoppingService } from '../../services/shopping/shopping.service';
+import { AuthService } from '../../services/auth/auth.service';
+import { ShoppingCart } from '../../models/ShoppingCart';
 
+
+class RouterStub {
+    navigate = jasmine.createSpy();
+}
+
+class AuthServiceStub {
+    logout = jasmine.createSpy();
+}
+
+class ShoppingServiceStub {
+    private shoppingCartSubject = new BehaviorSubject(null);
+
+    setShoppingCartMock(value) {
+        this.shoppingCartSubject.next(value);
+    }
+
+    get shoppingCart$() {
+        return this.shoppingCartSubject;
+    }
+}
 
 describe('LogoutComponent', () => {
+
     let component: LogoutComponent;
     let fixture: ComponentFixture<LogoutComponent>;
 
-    beforeEach(async(() => {
+    beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [ LogoutComponent ],
-        })
-            .compileComponents();
-    }));
-
-    beforeEach(() => {
+            providers: [
+                { provide: Router, useClass: RouterStub },
+                { provide: ShoppingService, useClass: ShoppingServiceStub },
+                { provide: AuthService, useClass: AuthServiceStub },
+            ],
+        });
         fixture = TestBed.createComponent(LogoutComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('should logout user on init, clear shopping cart and navigate the user to the /sign-in page', () => {
+        // given
+        const authService = TestBed.get(AuthService);
+        const shoppingService = TestBed.get(ShoppingService);
+        const router = TestBed.get(Router);
+        fixture.detectChanges();
+
+        // when/then
+        expect(authService.logout).toHaveBeenCalledTimes(1);
+        expect(shoppingService.shoppingCart$.value).toEqual(ShoppingCart.emptyShoppingCart);
+        expect(router.navigate).toHaveBeenCalledTimes(1);
+        expect(router.navigate).toHaveBeenCalledWith([ '/sign-in' ], { replaceUrl: true });
     });
+
 });
